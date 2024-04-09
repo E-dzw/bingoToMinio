41ed7ae122bc10ac67787ca3caf50b5a166df7a7
baitu.dzw <baitu@come-future.com>
2024-04-09 17:22:36 +0800

    remove superflues code

--- cmd/minio.go
+++ cmd/minio.go
@@ -59,21 +59,12 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// minioCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	// mysqlHost = minioCmd.Flags().String("mysqlHost", "127.0.0.1", "mysql server address")
-	// mysqlPort = minioCmd.Flags().Int("mysqlPort", 3306, "mysql server port")
-	// mysqlUser = minioCmd.Flags().String("mysqlUser", "", "mysql account")
-	// mysqlPassword = minioCmd.Flags().String("mysqlPassword", "", "mysql password")
 	minioCmd.Flags().StringVar(&minioPath, "minioPath", "/", "define a minio path to store binlog")
 	minioCmd.Flags().StringVar(&minioBucketName, "minioBucketName", "", "minio bucket name ")
 	minioCmd.Flags().StringVar(&minioHost, "minioHost", "127.0.0.1", "minio address")
 	minioCmd.Flags().IntVar(&minioPort, "minioPort", 9000, "minio s3 port")
 	minioCmd.Flags().StringVar(&minioAccessKey, "minioAccessKey", "", "minio AccessKey")
 	minioCmd.Flags().StringVar(&minioSecretKey, "minioSecretKey", "", "minio SecretKey")
-	// tmpPath = minioCmd.Flags().String("tmpPath", "./tmpPath", "define  a temporary local file path to store binlog from mysql server")
-	// conbackupNumber = minioCmd.Flags().Int("conbackupNumber", 1, "how many backup go routine to backup binlog to minio")
-	// startBinlog = minioCmd.Flags().String("startBinlog", "", "where from to start backup  ")
 	minioCmd.MarkFlagRequired("minioAccessKey")
 	minioCmd.MarkFlagRequired("minioSecretKey")
 	minioCmd.MarkFlagRequired("minioBucketName")
